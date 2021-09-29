2c34e5bb46be2a92b99399b5cce4b673993b98e2
asrar bhat <asrarpsi@gmail.com>
2021-09-29 17:35:17 +0530

    try basic js

---
+++ script.js
@@ -0,0 +1,46 @@
+// console.log("hello world")
+// alert("hi there")
+// var v = prompt("enter a sexy value")
+// console.log(v)
+
+
+// setTimeout(() => {
+//     console.log(x)
+// }, 3000)
+// let x = 33;
+// setTimeout(() => {
+//     console.log(x)
+//     x = 44
+// }, 2000)
+
+// console.log(hello())
+// function hello() {
+//     console.log("hi");
+//     return 0;
+// }
+
+// let name = "rayon"
+// let text =
+//     `hello there,
+// my nameis ${name}`;
+// console.log(text)
+
+// function rayon() {
+//     console.log(arguments[3])
+// }
+// rayon(1, 2, 3, "rayon")
+
+// rayon = () => {
+//     console.log("hello there")
+//     return () => console.log("hi there")
+// }
+// rayon()()
+
+// (() => console.log("hey there"))()
+'use strict'
+function rayon() {
+    value = 10;
+}
+
+rayon()
+console.log(window.value)
